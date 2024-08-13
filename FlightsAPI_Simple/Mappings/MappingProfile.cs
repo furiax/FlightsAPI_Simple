476d24ebfb8416bfd33f17d7779aea17b9c370e8
furiax <carlmalfliet@proximus.be>
2024-08-13 11:37:34 +0200

    installed AutoMapper and created a mappingprofile

---
+++ FlightsAPI_Simple/Mappings/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using FlightsAPI_Simple.Dtos;
+using FlightsAPI_Simple.Models;
+
+namespace FlightsAPI_Simple.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Flight, FlightApiRequestDto>();
+            CreateMap<FlightApiRequestDto, Flight>();
+        }
+    }
+}
