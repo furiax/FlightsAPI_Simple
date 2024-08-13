ffc20276ad9c3a9214edddb1137fd2c22286ee95
furiax <carlmalfliet@proximus.be>
2024-08-13 13:17:22 +0200

    created apiResponseDto

---
+++ FlightsAPI_Simple/Dtos/ApiResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace FlightsAPI_Simple.Dtos
+{
+    public class ApiResponseDto<T>
+    {
+        public bool RequestFailed { get; set; } = false;
+        public HttpStatusCode ResponseCode { get; set; }
+        public string ErrorMessage { get; set; } = string.Empty;
+        public T? Data { get; set; }
+    }
+}
