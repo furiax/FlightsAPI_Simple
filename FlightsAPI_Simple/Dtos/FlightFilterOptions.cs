2a6240a56efafbcfe990b390e2097c881247d85f
furiax <carlmalfliet@proximus.be>
2024-08-13 14:10:56 +0200

    Created a DTO for filtering

---
+++ FlightsAPI_Simple/Dtos/FlightFilterOptions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightsAPI_Simple.Dtos
+{
+    public class FlightFilterOptions
+    {
+        [FromQuery(Name = "airline_name")]
+        public string AirlineName { get; set; } = string.Empty;
+        [FromQuery(Name = "departure_airport_code")]
+        public string DepartureAirportCode { get; set; } = string.Empty;
+        [FromQuery(Name = "arrival_airport_code")]
+        public string ArrivalAirportCode { get; set; } = string.Empty;
+        [FromQuery(Name = "departure_date_time")]
+        public DateTime DepartureDateTime { get; set; }
+        [FromQuery(Name = "arrival_date_time")]
+        public DateTime ArrivalDateTime { get; set; }
+    }
+}
