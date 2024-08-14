d9885e17a86b04ef603e97732a1a2e6bf9faeb78
furiax <carlmalfliet@proximus.be>
2024-08-14 22:16:23 +0200

    added DbSet to context page added the one to many relationship between flight and seats adjusted the SeedData method to create some seats

--- FlightsAPI_Simple/Data/FlightsDbContext.cs
+++ FlightsAPI_Simple/Data/FlightsDbContext.cs
@@ -12,6 +12,8 @@ namespace FlightsAPI_Simple.Data
 
         public DbSet<Flight> Flights { get; set; }
         public DbSet<Airline> Airlines { get; set; }
+        public DbSet<Seat> Seats { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -20,7 +22,13 @@ namespace FlightsAPI_Simple.Data
                 .WithOne(a => a.Flight)
                 .HasForeignKey<Airline>(A => A.FlightId)
                 .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Seat>()
+                .HasOne(s => s.Flight)
+                .WithMany(f => f.Seats)
+                .HasForeignKey(s => s.FlightId);
         }
+
 
         public void SeedData()
         {
@@ -34,11 +42,43 @@ namespace FlightsAPI_Simple.Data
 
             Airlines.AddRange(airlines);
 
+            Seats.RemoveRange(Seats);
+            var seatsA = new List<Seat>
+            {
+                new Seat {SeatNumber = "A1"},
+                new Seat {SeatNumber = "A2"},
+                new Seat {SeatNumber = "A3"},
+                new Seat {SeatNumber = "A4"},
+                new Seat {SeatNumber = "A5"},
+                new Seat {SeatNumber = "A6"}
+            };
+            var seatsB = new List<Seat>
+            {
+                new Seat {SeatNumber = "B1"},
+                new Seat {SeatNumber = "B2"},
+                new Seat {SeatNumber = "B3"},
+                new Seat {SeatNumber = "B4"},
+                new Seat {SeatNumber = "B5"},
+                new Seat {SeatNumber = "B6"}
+            };
+            var seatsC = new List<Seat>
+            {
+                new Seat {SeatNumber = "C1"},
+                new Seat {SeatNumber = "C2"},
+                new Seat {SeatNumber = "C3"},
+                new Seat {SeatNumber = "C4"},
+                new Seat {SeatNumber = "C5"},
+                new Seat {SeatNumber = "C6"}
+            };
+            Seats.AddRange(seatsA);
+            Seats.AddRange(seatsB);
+            Seats.AddRange(seatsC);
+
             Flights.RemoveRange(Flights);
             Flights.AddRange(
-                new Flight { FlightNumber = "AA-101", DepartureAirportCode = "NYC", ArrivalAirportCode = "LON", DepartureDateTime = new DateTime(2024,5,24,21,40,0) , ArrivalDateTime = new DateTime(2024, 5, 25, 04, 00, 0), PassengerCapacity = 100, Airline = airlines[0] },
-                new Flight { FlightNumber = "AB-202", DepartureAirportCode = "PAR", ArrivalAirportCode = "TKY", DepartureDateTime = new DateTime(2024, 5, 27, 22, 40, 0), ArrivalDateTime = new DateTime(2024, 5, 28, 05, 13, 0), PassengerCapacity = 120, Airline = airlines[1] },
-                new Flight { FlightNumber = "AC-303", DepartureAirportCode = "SYD", ArrivalAirportCode = "LAX", DepartureDateTime = new DateTime(2024, 5, 28, 10, 22, 0), ArrivalDateTime = new DateTime(2024, 5, 28, 20, 50, 0), PassengerCapacity = 95, Airline = airlines[2] }
+                new Flight { FlightNumber = "AA-101", DepartureAirportCode = "NYC", ArrivalAirportCode = "LON", DepartureDateTime = new DateTime(2024,5,24,21,40,0) , ArrivalDateTime = new DateTime(2024, 5, 25, 04, 00, 0), PassengerCapacity = 100, Airline = airlines[0], Seats = seatsA },
+                new Flight { FlightNumber = "AB-202", DepartureAirportCode = "PAR", ArrivalAirportCode = "TKY", DepartureDateTime = new DateTime(2024, 5, 27, 22, 40, 0), ArrivalDateTime = new DateTime(2024, 5, 28, 05, 13, 0), PassengerCapacity = 120, Airline = airlines[1], Seats = seatsB },
+                new Flight { FlightNumber = "AC-303", DepartureAirportCode = "SYD", ArrivalAirportCode = "LAX", DepartureDateTime = new DateTime(2024, 5, 28, 10, 22, 0), ArrivalDateTime = new DateTime(2024, 5, 28, 20, 50, 0), PassengerCapacity = 95, Airline = airlines[2], Seats = seatsC }
                 );
             SaveChanges();
         }
