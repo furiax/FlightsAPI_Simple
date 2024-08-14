3d50d58b2f45f4000cbf80055a22e026f80b7e65
furiax <carlmalfliet@proximus.be>
2024-08-14 21:52:45 +0200

    adjusted context file: - added dbset for airlines - set up the 1to1 relationship - adjust seed data method

--- FlightsAPI_Simple/Data/FlightsDbContext.cs
+++ FlightsAPI_Simple/Data/FlightsDbContext.cs
@@ -11,14 +11,34 @@ namespace FlightsAPI_Simple.Data
         }
 
         public DbSet<Flight> Flights { get; set; }
+        public DbSet<Airline> Airlines { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Flight>()
+                .HasOne(f => f.Airline)
+                .WithOne(a => a.Flight)
+                .HasForeignKey<Airline>(A => A.FlightId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
 
         public void SeedData()
         {
+            Airlines.RemoveRange(Airlines);
+            var airlines = new List<Airline>
+            {
+                new Airline { Name = "Airline A" },
+                new Airline { Name = "Airline B" },
+                new Airline { Name = "Airline C" }
+            };
+
+            Airlines.AddRange(airlines);
+
             Flights.RemoveRange(Flights);
             Flights.AddRange(
-                new Flight { FlightNumber = "AA-101", AirlineName = "Airline A", DepartureAirportCode = "NYC", ArrivalAirportCode = "LON", DepartureDateTime = new DateTime(2024,5,24,21,40,0) , ArrivalDateTime = new DateTime(2024, 5, 25, 04, 00, 0), PassengerCapacity = 100 },
-                new Flight { FlightNumber = "AB-202", AirlineName = "Airline B", DepartureAirportCode = "PAR", ArrivalAirportCode = "TKY", DepartureDateTime = new DateTime(2024, 5, 27, 22, 40, 0), ArrivalDateTime = new DateTime(2024, 5, 28, 05, 13, 0), PassengerCapacity = 120 },
-                new Flight { FlightNumber = "AC-303", AirlineName = "Airline C", DepartureAirportCode = "SYD", ArrivalAirportCode = "LAX", DepartureDateTime = new DateTime(2024, 5, 28, 10, 22, 0), ArrivalDateTime = new DateTime(2024, 5, 28, 20, 50, 0), PassengerCapacity = 95 }
+                new Flight { FlightNumber = "AA-101", DepartureAirportCode = "NYC", ArrivalAirportCode = "LON", DepartureDateTime = new DateTime(2024,5,24,21,40,0) , ArrivalDateTime = new DateTime(2024, 5, 25, 04, 00, 0), PassengerCapacity = 100, Airline = airlines[0] },
+                new Flight { FlightNumber = "AB-202", DepartureAirportCode = "PAR", ArrivalAirportCode = "TKY", DepartureDateTime = new DateTime(2024, 5, 27, 22, 40, 0), ArrivalDateTime = new DateTime(2024, 5, 28, 05, 13, 0), PassengerCapacity = 120, Airline = airlines[1] },
+                new Flight { FlightNumber = "AC-303", DepartureAirportCode = "SYD", ArrivalAirportCode = "LAX", DepartureDateTime = new DateTime(2024, 5, 28, 10, 22, 0), ArrivalDateTime = new DateTime(2024, 5, 28, 20, 50, 0), PassengerCapacity = 95, Airline = airlines[2] }
                 );
             SaveChanges();
         }
