408bc12dc39cd215733d3d884602ab1c1cc0aeda
furiax <carlmalfliet@proximus.be>
2024-08-14 22:04:34 +0200

    created new model Seat

---
+++ FlightsAPI_Simple/Models/Seat.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightsAPI_Simple.Models
+{
+    public class Seat
+    {
+        [Key]
+        public int Id { get; set; }
+        public string SeatNumber { get; set; } = string.Empty;
+        public int FlightId { get; set; }
+        public virtual Flight Flight { get; set; }
+    }
+}
